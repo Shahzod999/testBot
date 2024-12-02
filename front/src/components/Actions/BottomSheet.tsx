998d9ca25ee715a23203ad44cad53ea2557abadb
Shahzod <sibragimov999@gmail.com>
2024-12-02 16:06:50 +0500

    drag

--- front/src/components/Actions/BottomSheet.tsx
+++ front/src/components/Actions/BottomSheet.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import "./bottomSheet.scss";
 
 interface BottomSheetProps {
@@ -8,13 +8,41 @@ interface BottomSheetProps {
 }
 
 const BottomSheet = memo(({ isOpen, onClose, children }: BottomSheetProps) => {
+  const [startY, setStartY] = useState<number | null>(null);
+  const [currentY, setCurrentY] = useState<number | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setStartY(e.touches[0].clientY);
+    setIsDragging(false);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    if (startY === null) return;
+    const touchY = e.touches[0].clientY;
+    setCurrentY(touchY);
+    if (touchY - startY > 0) {
+      setIsDragging(true);
+    }
+  };
+
+  const handleTouchEnd = () => {
+    if (startY !== null && currentY !== null && currentY - startY > 100) {
+      onClose();
+    }
+
+    setStartY(null);
+    setCurrentY(null);
+    setIsDragging(false);
+  };
+
+  console.log(isDragging);
+
   return (
     <>
-      <div
-        className={`bottom__wrapper ${isOpen ? "bottom__wrapper--active" : ""}`}
-        onClick={onClose}></div>
+      <div className={`bottom__wrapper ${isOpen ? "bottom__wrapper--active" : ""}`} onClick={onClose}></div>
 
-      <div className={`bottom-sheet ${isOpen ? "bottom-sheet--open" : ""}`}>
+      <div className={`bottom-sheet ${isOpen ? "bottom-sheet--open" : ""} ${isDragging ? "bottom-sheet--dragging" : ""}`} draggable={false} onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
         <div className="bottom-sheet__content">
           <div className="bottom-sheet__close" onClick={onClose}>
             <div></div>
