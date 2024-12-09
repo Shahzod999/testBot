12cb7f05818e374a08df17c467e11e54400a3545
Shahzod <sibragimov999@gmail.com>
2024-12-09 06:22:50 +0500

    raiting

--- front/src/components/raiting/AddComment/TextArea.tsx
+++ front/src/components/raiting/AddComment/TextArea.tsx
@@ -10,11 +10,31 @@ const TextArea = ({ text, setText }: TextProps) => {
   const getProgressState = () => {
     const length = text.trim().length;
 
-    if (length === 0) return { state: "low", width: 0 };
-    if (length < 10) return { state: "low", width: (length / maxLength) * 100 };
-    if (length < 30) return { state: "medium", width: (length / maxLength) * 100 };
-    if (length < 50) return { state: "nice", width: (length / maxLength) * 100 };
-    return { state: "good", width: 100 };
+    if (length === 0)
+      return { state: "low", width: 0, text: "Это слишком коротко." };
+    if (length < 10)
+      return {
+        state: "low",
+        width: (length / maxLength) * 100,
+        text: "Это слишком коротко.",
+      };
+    if (length < 30)
+      return {
+        state: "medium",
+        width: (length / maxLength) * 100,
+        text: "Это слишком коротко.",
+      };
+    if (length < 50)
+      return {
+        state: "nice",
+        width: (length / maxLength) * 100,
+        text: "Выглядит хорошо. Добавьте ещё несколько слов.",
+      };
+    return {
+      state: "good",
+      width: 100,
+      text: "Замечательно. Поделитесь своим опытом.",
+    };
   };
 
   const progress = getProgressState();
@@ -25,21 +45,15 @@ const TextArea = ({ text, setText }: TextProps) => {
         rows={5}
         placeholder="Расскажите нам про это место"
         value={text}
-        onChange={(e) => setText(e.target.value)}
-      ></textarea>
+        onChange={(e) => setText(e.target.value)}></textarea>
 
       <div className="addComment__textArea__progress">
         <div className="progress-indicator">
           <div
             className={`progress-bar ${progress.state}`}
-            style={{ width: `${progress.width}%` }}
-          ></div>
+            style={{ width: `${progress.width}%` }}></div>
         </div>
-        <p className="progress-message">
-          {progress.state === "good"
-            ? "Замечательно. Вы достигли максимума!"
-            : "Добавьте ещё символы, чтобы улучшить текст."}
-        </p>
+        <p className="progress-message">{progress.text}</p>
       </div>
     </div>
   );
