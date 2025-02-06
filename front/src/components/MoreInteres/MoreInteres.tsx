ae007e17574d568603a05365558e36c05ac7959c
Shahzod <sibragimov999@gmail.com>
2025-02-07 04:36:59 +0500

    framer

--- front/src/components/MoreInteres/MoreInteres.tsx
+++ front/src/components/MoreInteres/MoreInteres.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGetMenuQuery } from "../../app/api/menuSlice";
 import { CompanyState } from "../../app/types/companyType";
@@ -10,19 +10,33 @@ const MoreInteres = ({ companyInfo }: { companyInfo: CompanyState }) => {
   const navigate = useNavigate();
   const { data: menuData, isFetching } = useGetMenuQuery({
     company_id: companyInfo?._id,
-    limit: 4,
+    limit: 3,
   });
 
+  const [stretch, setStretch] = useState(0);
+  const MAX_STRETCH = 1; // Максимальное увеличение (1.5x)
+
   const containerRef = useRef<HTMLDivElement>(null);
+  const nextItemRef = useRef<HTMLDivElement>(null);
 
   const handleScroll = () => {
     const container = containerRef.current;
-    if (container) {
-      const { scrollLeft, scrollWidth, clientWidth } = container;
-      const isEnd = Math.abs(scrollLeft + clientWidth - scrollWidth) < 5; // Погрешность 5px
+    const nextItem = nextItemRef.current;
 
-      if (isEnd) {
-        navigate("/menu");
+    if (container && nextItem) {
+      const { scrollLeft, scrollWidth, clientWidth } = container;
+
+      const overScroll = scrollLeft + clientWidth - scrollWidth;
+
+      if (overScroll > -100) {
+        const stretchFactor = Math.min(1 + overScroll / 100, MAX_STRETCH);
+        setStretch(stretchFactor);
+
+        if (stretchFactor >= MAX_STRETCH) {
+          navigate("/menu");
+        }
+      } else if (stretch > 1) {
+        setStretch(1);
       }
     }
   };
@@ -39,6 +53,16 @@ const MoreInteres = ({ companyInfo }: { companyInfo: CompanyState }) => {
         {menuData?.data?.map((food: MenuType) => (
           <FoodBox food={food} key={food._id} isFetching={isFetching} />
         ))}
+
+        <div
+          ref={nextItemRef}
+          className="moreInteres__wrapper__next"
+          style={{
+            transform: `scale(${stretch})`,
+            transition: stretch === 1 ? "transform 0.3s ease-out" : "none",
+          }}>
+          Далее
+        </div>
       </div>
     </div>
   );
