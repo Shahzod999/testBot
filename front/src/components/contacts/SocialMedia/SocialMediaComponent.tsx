f2dd56d917a2b3003d41c119b4afc717fe6abb37
Shahzod <sibragimov999@gmail.com>
2025-02-13 15:25:23 +0500

    tgLink

--- front/src/components/contacts/SocialMedia/SocialMediaComponent.tsx
+++ front/src/components/contacts/SocialMedia/SocialMediaComponent.tsx
@@ -14,7 +14,7 @@ const SocialMediaComponent = ({
         .map(([name, url]) => {
           const link =
             name.toLowerCase() === "telegram"
-              ? `t.me/${url.replace("@", "")}`
+              ? `https://t.me/${url.replace("@", "")}`
               : name.toLowerCase() === "whatsapp"
               ? `https://wa.me/${url.replace("+", "").trim()}`
               : url;
