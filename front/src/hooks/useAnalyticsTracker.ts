b35813a815f5b76609177b8316b16d6efb78b44e
Shahzod <sibragimov999@gmail.com>
2025-03-12 01:11:10 +0500

    total

--- front/src/hooks/useAnalyticsTracker.ts
+++ front/src/hooks/useAnalyticsTracker.ts
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../app/store";
 import {
   trackEvent,
@@ -7,32 +6,44 @@ import {
   AnalyticsState,
 } from "../app/features/analyticsSlice";
 import { useUpdateAnalyticsMutation } from "../app/api/companySlice";
+import { useAppDispatch, useAppSelector } from "./reduxHooks";
 
 const useAnalyticsTracker = (companyId: string) => {
-  const dispatch = useDispatch();
-  const analyticsData = useSelector((state: RootState) => state.analytics);
+  const dispatch = useAppDispatch();
+  const analyticsData = useAppSelector((state: RootState) => state.analytics);
   const [updateAnalytics] = useUpdateAnalyticsMutation();
   const timeoutRef = useRef<any>(null);
 
   const track = (event: keyof AnalyticsState) => {
     dispatch(trackEvent({ event }));
 
+    const updatedAnalytics = {
+      ...analyticsData,
+      [event]: (analyticsData[event] || 0) + 1,
+    };
+
     if (timeoutRef.current) clearTimeout(timeoutRef.current);
 
     timeoutRef.current = setTimeout(() => {
-      sendAnalytics();
+      sendAnalytics(updatedAnalytics);
     }, 10_00);
   };
 
-  const sendAnalytics = async () => {
+  const sendAnalytics = async (updatedData: AnalyticsState) => {
     if (!companyId) return;
 
     try {
       let res = await updateAnalytics({
         company_id: companyId,
-        data: analyticsData,
+        data: updatedData,
       }).unwrap();
-      console.log(res);
+      console.log(
+        {
+          company_id: companyId,
+          data: updatedData,
+        },
+        "analytics",
+      );
 
       if (res?.status == "success") {
         dispatch(resetAnalytics());
